4b3a019352032e6c237b8831abcfa4bc1942509b
Arman Mir <mirarman8583@gmail.com>
2025-09-24 08:17:01 +0600

    State management and toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,37 @@ const fetchPlayers = async () => {
 
 function App() {
   const playersPromise = fetchPlayers();
+
+  const [toggle, setToggle] = useState(true)
+
   return (
     <>
 
       <Navbar></Navbar>
-      
+      <div className=' max-w-[1200px] mx-auto flex justify-between items-center ' >
+        <h1 className=' font-bold text-2xl ' >Available Players</h1>
 
-      <Suspense fallback={<span className="loading loading-dots loading-xl"></span> }>
-        <AvailablePlayers playersPromise={playersPromise} ></AvailablePlayers>
-      </Suspense>
+        <div className=' font-bold ' >
+          <button onClick={() => setToggle(true)} className={`' py-3 px-4 border-1 border-gray-400 rounded-l-2xl border-r-0 ${toggle===true?"bg-[#E7FE29] text-black":"" } `} >Available</button>
 
-      <SelectedPlayers></SelectedPlayers>
+          <button onClick={() => setToggle(false)} className={`py-3 px-4 border-1 border-gray-400 rounded-r-2xl border-l-0 ${toggle===false?"bg-[#E7FE29] text-black":""} `} >Selected <span>0</span> </button>
+        </div>
+
+      </div>
+
+      {
+        toggle === true ? 
+
+        <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}>
+          <AvailablePlayers playersPromise={playersPromise} ></AvailablePlayers>
+        </Suspense> : 
+        
+        <SelectedPlayers></SelectedPlayers>
+      }
+
+
+
+
 
 
 
